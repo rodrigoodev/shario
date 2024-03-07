aa3c09a9f86f19dbdee27387d6b3e3f1285475b7
rodrigorgrb@gmail.com <rodrigorgrb@gmail.com>
2024-03-07 01:17:17 -0300

    burn: remove runtime

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["nuxt-primevue", "@nuxtjs/supabase", "@pinia/nuxt"],
-  runtimeConfig: {
-    public: {
-      baseUrl: process.env.BASE_URL || "http://localhost:3000",
-    },
-  },
+
   primevue: {
     /* Options */
   },
